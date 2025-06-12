7c37a93bd2a52b0726306f9f878899eccad0d155
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-06-12 20:23:08 +0530

    update: higher order function

--- tutorial-01/higher_order_function.js
+++ tutorial-01/higher_order_function.js
@@ -1,3 +1,12 @@
+function copyArrayAndMultiplyBy2(array) {
+    const output = [];
+    for (let i = 0; i < array.length; i++) {
+        output.push(array[i] * 2);
+    }
+
+    return output;
+}
+
 function copyArrayAndDivideBy2(array) {
     const output = [];
     for (let i = 0; i < array.length; i++) {
@@ -7,5 +16,25 @@ function copyArrayAndDivideBy2(array) {
     return output;
 }
 
+// do the same thing but this time add 3 to array elements
+
+function copyArrayAndManipulate(array, instruction) {
+    const output = [];
+    for (let i = 0; i < array.length; i++) {
+        output.push(instruction(array[i]));
+    }
+    return output;
+}
+
+function multiplyBy2(input) {
+    return input * 2;
+}
+
+function add3(input) {
+    return input + 3;
+}
+
 const myArray = [1, 2, 3];
-const result = copyArrayAndDivideBy2(myArray);
+const result = copyArrayAndManipulate(myArray, add3);
+
+console.log(result);
